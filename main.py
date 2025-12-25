2f7b7b0ec2523b008a2f63b9e3403cdec58162f0
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-12-25 16:20:30 +0100

    Add confusion matrix and classification report outputs

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+
 
 clf = LogisticRegression()
 
@@ -7,3 +10,7 @@ clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 
 y_probs = clf.predict_proba(X_test)[:, 1]
+
+print(confusion_matrix(y_test, y_pred))
+
+print(classification_report(y_test, y_pred))
